30f8acafd2f2cef162e3890885eea5469b519525
O-Jun-S <jun.shima256@gmail.com>
2021-09-12 09:16:54 +0900

    Write first cli application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::env;
+use seahorse::{App, Command, Context};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let app = App::new()
+        .name(env!("CARGO_PKG_NAME"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .usage("cli_tool [command] [x] [y]")
+        .command(
+            Command::new()
+            .name("t")
+            .usage("ob64 t {}")
+            .action(t),
+        );
+    app.run(args);
 }
+
+fn t(_c: &Context) {
+    let t = "Hello, World";
+    println!("{}", t);
+}
+
