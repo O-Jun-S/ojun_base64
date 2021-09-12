2f003b32f4e411332b0b297624cc381cff4afb28
O-Jun-S <jun.shima256@gmail.com>
2021-09-12 09:34:30 +0900

    change usage.

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,24 @@ fn main() {
         .name(env!("CARGO_PKG_NAME"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .version(env!("CARGO_PKG_VERSION"))
-        .usage("cli_tool [command] [x] [y]")
+        .usage("cli_tool [command] [target]")
         .command(
             Command::new()
             .name("t")
             .usage("ob64 t {}")
             .action(t),
+        )
+        .command(
+            Command::new()
+            .name("e")
+            .usage("ob64 e {}")
+            .action(e),
+        )
+        .command(
+            Command::new()
+            .name("d")
+            .usage("ob64 d {}")
+            .action(d),
         );
     app.run(args);
 }
@@ -20,5 +32,15 @@ fn main() {
 fn t(_c: &Context) {
     let t = "Hello, World";
     println!("{}", t);
+}
+
+fn e(c: &Context) {
+    println!("{}", base64::encode(&c.args[0]));
+}
+
+fn d(c: &Context) {
+    let by = base64::decode(&c.args[0]).expect("Couldn't decode it!");
+    let res = by.iter().map(|&s| s as char).collect::<String>();
+    println!("{:?}", res);
 }
 
